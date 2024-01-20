4902a1dc2e7462b6a43b5583f999c9d59fda366d
Tanveer Najib <tanveernajib@gmail.com>
2024-01-20 16:11:22 +0000

    Ignore me

--- al2ka.py
+++ al2ka.py
@@ -44,7 +44,7 @@ def main():
     """Entry point"""
     volumes = []
 
-    with open("json/AssClass.json", "r", encoding="utf8") as file:
+    with open("json/Skullface.json", "r", encoding="utf8") as file:
         contents = file.read()
         jayson = json.loads(contents)
         mediaid = jayson["id"]
--- al2ka.rb
+++ al2ka.rb
@@ -65,8 +65,8 @@ end
 def main
     volumes = []
 
-    kaboom_id = 41
-    uri = URI("https://raw.githubusercontent.com/crxssed7/al3ka/main/json/TokyoGhoul.json")
+    kaboom_id = 19
+    uri = URI("https://raw.githubusercontent.com/crxssed7/al3ka/main/json/AssClass.json")
     response = Net::HTTP.get_response(uri)
     return unless response.is_a? Net::HTTPSuccess
 
