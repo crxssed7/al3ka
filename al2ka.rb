bc86d2374116c4f5093d7af6c57c3567b21aef91
Tanveer Najib <tanveernajib@gmail.com>
2024-01-20 19:59:06 +0000

    Ignore me

--- al2ka.rb
+++ al2ka.rb
@@ -65,8 +65,8 @@ end
 def main
     volumes = []
 
-    kaboom_id = 79
-    uri = URI("https://raw.githubusercontent.com/crxssed7/al3ka/main/json/Colourless.json")
+    kaboom_id = 23
+    uri = URI("https://raw.githubusercontent.com/crxssed7/al3ka/main/json/SakamotoDays.json")
     response = Net::HTTP.get_response(uri)
     return unless response.is_a? Net::HTTPSuccess
 
